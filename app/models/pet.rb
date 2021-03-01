5038d32224aadd680631889498f7c11ac1d72db6
ClintonEnyinna <enyinnaclinton@gmail.com>
2021-02-28 23:34:50 -0600

    annotate pet and user models

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: pets
+#
+#  id          :bigint           not null, primary key
+#  age         :integer
+#  breed       :string
+#  medications :string
+#  name        :string
+#  sickness    :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  chip_id     :string
+#  user_id     :bigint           not null
+#
+# Indexes
+#
+#  index_pets_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class Pet < ApplicationRecord
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id           :bigint           not null, primary key
+#  address      :string
+#  email        :string
+#  first_name   :string
+#  general_info :string
+#  last_name    :string
+#  phone        :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 class User < ApplicationRecord
   has_many :pets
 end
